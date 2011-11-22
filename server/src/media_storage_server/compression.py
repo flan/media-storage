90fcfdf826102a3ead6091b6307f99c5e1d26143
Neil Tallim <flan@uguu.ca>
2011-11-21 22:18:35 -0700

    Fleshing out the structure.

--- server/src/media_storage_server/compression.py
+++ server/src/media_storage_server/compression.py
@@ -2,3 +2,19 @@
 Offers efficient handlers for compressing and decompressing data, working with
 file-like objects (often tempfiles).
 """
+import bz2
+import gzip
+
+FORMATS = ['bz2', 'gzip']
+
+try:
+    import lzma
+except Exception as e:
+    _log.write('Unable to add support for LZMA: ' + str(e))
+    lzma = None
+else:
+    FORMATS.append('lzma')
+    
+FORMATS = tuple(FORMATS)
+
+
