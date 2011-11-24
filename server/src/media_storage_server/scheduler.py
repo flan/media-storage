df63c7f4bd7c53bf6576f048fe9a8d43a08c1b77
Neil Tallim <flan@uguu.ca>
2011-11-23 20:53:07 -0700

    Defined the second thread.

--- server/src/media_storage_server/scheduler.py
+++ server/src/media_storage_server/scheduler.py
@@ -8,35 +8,90 @@ import time
 import database
 import state
 
-class DeletionExecutor(threading.Thread):
+class DeletionMaintainer(threading.Thread):
+    """
+    Scheduling format:
+    maintainence.deletion = sa[22:00..23:59] su[0:00..2:00,2:30..4:30]
+    maintainence.deletion_sleep = 300
+    """
+    def __init__(self)
+        threding.Thread.__init__(self)
+        self.daemon = True
+        
     def run(self):
         #sleep until window starts
         #Retain position between runs, cycling to the start only once all records have
         #been addressed
         while True:
-            time.sleep(60)
+            time.sleep(CONFIG.maintainence__deletion_sleep)
             
-class CompressionExecutor(threading.Thread):
+class CompressionMaintainer(threading.Thread):
+    """
+    Scheduling format:
+    maintainence.compression = sa[22:00..23:59] su[0:00..2:00,2:30..4:30]
+    maintainence.compression_sleep = 1800
+    """
+    def __init__(self)
+        threding.Thread.__init__(self)
+        self.daemon = True
+        
     def run(self):
         #sleep until window starts
         #Retain position between runs, cycling to the start only once all records have
         #been addressed
         while True:
-            time.sleep(60)
+            time.sleep(CONFIG.maintainence__compression_sleep)
             
-class DatabasePurger(threading.Thread):
+class DatabaseMaintainer(threading.Thread):
+    """
+    Scheduling format:
+    maintainence.database = sa[22:00..23:59] su[0:00..2:00,2:30..4:30]
+    maintainence.database_sleep = 43200
+    """
+    def __init__(self)
+        threding.Thread.__init__(self)
+        self.daemon = True
+        
     def run(self):
         """
         Cycles through every database record in order, removing any records associated
         with files that do not exist.
         """
-        #sleep until window starts
-        #Retain position between runs, cycling to the start only once all records have
-        #been addressed
+        ctime = -1.0
         while True:
-            time.sleep(60)
-            
-class FilesystemPurger(threading.Thread):
+            while False:
+                time.sleep(60)
+                
+            records_retrieved = False
+            for record in database.enumerate_all(ctime):
+                ctime = record['physical']['ctime']
+                records_retrieved = True
+                
+                filesystem = state.get_filesystem(record['physical']['family'])
+                if not filesystem.file_exists(record):
+                    database.drop_record(record['_id'])
+                    #TODO: log
+                    
+            if not records_retrieved: #Cycle complete
+                time.sleep(CONFIG.maintainence__database_sleep)
+                ctime = -1.0
+                
+class FilesystemMaintainer(threading.Thread):
+    """
+    This thread should be disabled by default, since it would allow for the deletion of
+    all data if the Mongo database is dropped for any reason, and, in smaller
+    data-centres, a full filesystem backup may not exist.
+    
+    Scheduling format:
+    maintainence.filesystem =
+    maintainence.filesystem_sleep = 43200
+    
+    If no parameters are given, the thread isn't even instantiated.
+    """
+    def __init__(self)
+        threding.Thread.__init__(self)
+        self.daemon = True
+        
     def run(self):
         """
         Cycles through every filesystem entry in order, removing any files associated
@@ -44,9 +99,12 @@ class FilesystemPurger(threading.Thread):
         """
         while True:
             for family in state.get_families():
+                #TODO: log
                 filesystem = state.get_filesystem(family)
                 self._walk(filesystem, './')
-                                
+            #Cycle complete
+            time.sleep(CONFIG.maintainence__filesystem_sleep)
+            
     def _walk(self, filesystem, path):
         try:
             for filename in filesystem.lsdir(path):
