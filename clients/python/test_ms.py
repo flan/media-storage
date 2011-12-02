ecc5bca7003aa28e2d0fc3aed3c15073b5677427
Neil Tallim <n.tallim@ivrnet.com>
2011-12-02 11:57:00 -0700

    Replacing old test code with a simpler, more unit-test-like example.

---
+++ clients/python/test_ms.py
@@ -0,0 +1,45 @@
+import media_storage
+
+print "Storing file..."
+x = media_storage.Client('http://localhost:1234/')
+y = x.put(open('/home/flan/test.wav'), 'audio/x-wav')
+print(repr(y))
+print
+
+print "Retrieving storage record..."
+print(repr(x.describe(y['uid'], y['keys']['read'])))
+print
+
+print "Retrieving content..."
+(mime, handle) = x.get(y['uid'], y['keys']['read'])
+print "Mime: " + mime
+print "Content: " + (open('/home/flan/test.wav').read() == handle.read() and 'match' or 'non-match')
+print
+
+print "Updating metadata..."
+x.update(y['uid'], y['keys']['write'], new={
+ 'hello': 'goodbye',
+ 'green': 5,
+})
+print
+
+print "Retrieving storage record..."
+print(repr(x.describe(y['uid'], y['keys']['read'])))
+print
+
+query = media_storage.QueryStruct()
+query.meta['green'] = 5
+print "Querying for matches ('green' = 5)..."
+print(repr(x.query(query)))
+print
+
+print "Unlinking entity..."
+x.unlink(y['uid'], y['keys']['write'])
+print
+
+print "Querying for matches ('green' = 5)..."
+print(repr(x.query(query)))
+print
+
+print "Done"
+
