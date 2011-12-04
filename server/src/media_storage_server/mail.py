9a41527da8b1d3ed0b64d34e52d7b69669dcab79
Neil Tallim <flan@uguu.ca>
2011-12-03 20:19:42 -0700

    Added config interfaces.

--- server/src/media_storage_server/mail.py
+++ server/src/media_storage_server/mail.py
@@ -1,3 +1,31 @@
+"""
+media-storage : mail
+====================
+
+Offers e-mail handling for important alerts.
+
+This module is shared by every server facet of the media-storage project and
+changes to one instance should be reflected in all.
+ 
+Legal
+-----
+
+This file is part of the media-storage project.
+This module is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published
+by the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+ 
+(C) Neil Tallim, 2011
+"""
 import logging
 from email.mime.text import MIMEText
 import smtplib
