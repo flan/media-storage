2750ed2cf97e028bf0382927ff70ac5acd45561d
Neil Tallim <flan@uguu.ca>
2011-12-05 20:35:01 -0700

    More visibility.

--- clients/python/media_storage/__init__.py
+++ clients/python/media_storage/__init__.py
@@ -4,6 +4,7 @@ Exposes implemented classes as the front-end for the library.
 from compression import (
  COMPRESS_NONE, COMPRESS_BZ2, COMPRESS_GZIP, COMPRESS_LZMA,
 )
+COMPRESSION_FORMATS = (COMPRESS_NONE, COMPRESS_BZ2, COMPRESS_GZIP, COMPRESS_LZMA,)
 
 from interfaces import QueryStruct
 from client import Client
