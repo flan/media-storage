563e2bbbae2fe955ab50fc8c9187ed2094e38b52
Neil Tallim <flan@uguu.ca>
2011-12-12 10:44:45 -0700

    Fully documented the client.

--- clients/python/media_storage/caching_proxy.py
+++ clients/python/media_storage/caching_proxy.py
@@ -1,5 +1,34 @@
 """
-Provides an implementation for the caching proxy client.
+media_storage.caching_proxy
+===========================
+
+Provides an implementation for a client that works with a local caching proxy.
+
+Usage
+-----
+
+This module is not meant to be used externally; relevant objects are exported
+to the package level.
+
+Legal
+-----
+
+This file is part of the LGPLed Python client of the media-storage project.
+This package is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU General Public License and
+GNU Lesser General Public License along with this program. If not, see
+<http://www.gnu.org/licenses/>.
+ 
+(C) Neil Tallim, 2011
 """
 import json
 
@@ -7,13 +36,25 @@ import interfaces
 import compression
 
 class CachingProxyClient(interfaces.RetrievalConstruct):
-    #When a proxy first gets a get or describe for an uncached record, it performs both actions,
-    #since it needs meta anyway, with the describe coming first. If there is no cache metadata,
-    #the get is not performed, unless a get was explicitly requested.
+    """
+    Implements the retrieval mechanisms of the client specification such that all interaction takes
+    place with a server running on the same host as the client, with that server being responsbible
+    for maintaining a local cache of requested data, to reduce overall network load and increase
+    response times for high-demand resources.
     
-    #Cached content must be separated into directories based on server name: whee.uguu.ca_8080/
-    
+    All operations performed by an instance of this class are atomic and instantaneous, but they
+    incur some additional local server load, meaning that it is a poor choice for data that is
+    accessed infrequently, but a good choice for templates and attachments that are sent out
+    repeatedly during campaigns.
+    """
     def __init__(self, server_host, server_port, proxy_port):
+        """
+        `server_host` and `server_port` indicate the address of the server to be used for
+        operations; the port must be an integer and the host may be an IP or name.
+        
+        `proxy_port` is the port on which the caching proxy is listening on the 'localhost'
+        interface.
+        """
         self._server_host = server_host
         self._server_port = server_port
         self._proxy = 'http://localhost:%(port)i/' % {
@@ -22,17 +63,16 @@ class CachingProxyClient(interfaces.RetrievalConstruct):
         
     def get(self, uid, read_key, output_file=None, decompress_on_server=False, timeout=5.0):
         """
-        Retrieves the requested data from the cache, returning its MIME and the decompressed
-        content as an open filehandle on the cached file in a tuple.
+        Retrieves the requested data from the local proxy, returning its MIME and the decompressed
+        content as an open filehandle to an on-disk file in a tuple.
         
-        `uid` and `read_key` are used to access the requested data.
+        `output_file` is ignored.
         
-        `output_file` is ignored because the actual file instance is returned.
+        `decompress_on_server` is ignored; the caching proxy is responsible for that.
         
-        `decompress_on_server` is ignored, since the caching proxy always tries to do local
-        decompression in the middle.
+        `timeout` defaults to 5.0s.
         
-        `timeout` is the number of seconds to allow for retrieval to complete. Defaults to 5.0s.
+        All other arguments are the same as in ``media_storage.interfaces.RetrievalConstruct.get``.
         """
         request = common.assemble_request(self._proxy + common.SERVER_GET, {
          'uid': uid,
@@ -52,11 +92,12 @@ class CachingProxyClient(interfaces.RetrievalConstruct):
         
     def describe(self, uid, read_key, timeout=2.5):
         """
-        Retrieves the requested record from the server as a dictionary.
+        Retrieves the requested record from the local proxy as a dictionary.
         
-        `uid` and `read_key` are used to access the requested data.
+        `timeout` defaults to 2.5s.
         
-        `timeout` is the number of seconds to allow for retrieval to complete. Defaults to 2.5s.
+        All other arguments are the same as in
+        ``media_storage.interfaces.ControlConstruct.describe``.
         """
         request = common.assemble_request(self._proxy + common.SERVER_DESCRIBE, {
          'uid': uid,
