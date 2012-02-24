2ccb385a5bc5fd75a4b6d6c94ce136c065adb8c3
Neil Tallim <n.tallim@ivrnet.com>
2012-02-23 18:31:41 -0700

    All exceptions now defined; just waiting for logic to be ported.

---
+++ clients/c-sharp/Exceptions.cs
@@ -0,0 +1,97 @@
+// 
+//  Exceptions.cs
+//  
+//  Author:
+//       Neil Tallim <flan@uguu.ca>
+// 
+//  Copyright (c) 2012 Neil Tallim
+// 
+//  This library is free software{} you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as
+//  published by the Free Software Foundation{} either version 2.1 of the
+//  License, or (at your option) any later version.
+// 
+//  This library is distributed in the hope that it will be useful, but
+//  WITHOUT ANY WARRANTY{} without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//  Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library{} if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+using System;
+
+namespace MediaStorage{
+    /// <summary>
+    /// The base class from which all errors native to this assembly inherit.
+    /// </summary>
+    public class Error : System.Exception{
+        public Error() : base(){}
+        public Error(string message) : base(message){}
+        public Error(string message, Exception innerException) : base(message, innerException){}
+    }
+
+    /// <summary>
+    /// Indicates a problem with the HTTP exchange.
+    /// </summary>
+    public class HttpError : Error{
+        public HttpError() : base(){}
+        public HttpError(string message) : base(message){}
+        public HttpError(string message, Exception innerException) : base(message, innerException){}
+    }
+
+    /// <summary>
+    /// The server returned a 403.
+    /// </summary>
+    public class NotAuthorisedError : HttpError{
+        public NotAuthorisedError() : base(){}
+        public NotAuthorisedError(string message) : base(message){}
+        public NotAuthorisedError(string message, Exception innerException) : base(message, innerException){}
+    }
+
+    /// <summary>
+    /// The server returned a 404.
+    /// </summary>
+    public class NotFoundError : HttpError{
+        public NotFoundError() : base(){}
+        public NotFoundError(string message) : base(message){}
+        public NotFoundError(string message, Exception innerException) : base(message, innerException){}
+    }
+
+    /// <summary>
+    /// The server returned a 409.
+    /// </summary>
+    public class InvalidRecordError : HttpError{
+        public InvalidRecordError() : base(){}
+        public InvalidRecordError(string message) : base(message){}
+        public InvalidRecordError(string message, Exception innerException) : base(message, innerException){}
+    }
+
+    /// <summary>
+    /// The server returned a 412.
+    /// </summary>
+    public class InvalidHeadersError : HttpError{
+        public InvalidHeadersError() : base(){}
+        public InvalidHeadersError(string message) : base(message){}
+        public InvalidHeadersError(string message, Exception innerException) : base(message, innerException){}
+    }
+
+    /// <summary>
+    /// The server returned a 503.
+    /// </summary>
+    public class TemporaryFailureError : HttpError{
+        public TemporaryFailureError() : base(){}
+        public TemporaryFailureError(string message) : base(message){}
+        public TemporaryFailureError(string message, Exception innerException) : base(message, innerException){}
+    }
+
+    /// <summary>
+    /// Indicates an error with URL resolution.
+    /// </summary>
+    public class URLError : Error{
+        public URLError() : base(){}
+        public URLError(string message) : base(message){}
+        public URLError(string message, Exception innerException) : base(message, innerException){}
+    }
+}
