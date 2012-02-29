ee8314fba5ebcdae5814690091755ded50384d60
Neil Tallim <n.tallim@ivrnet.com>
2012-02-28 17:41:16 -0700

    Implemented CachingProxy.

--- clients/c-sharp/StorageProxy.cs
+++ clients/c-sharp/StorageProxy.cs
@@ -19,7 +19,7 @@
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 namespace MediaStorage{
-    /*public class StorageProxy : Interfaces.StorageConstruct{
+    /*public class StorageProxy : BaseClient, Interfaces.StorageConstruct{
         public StorageProxy(){
         }
     }*/
