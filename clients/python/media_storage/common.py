63ee2295cc0dcefd072cd12b06e47410319473af
flan <flan@localhost>
2012-04-09 15:52:45 -0600

    Apparently, 2.6 didn't use namedtuples for version_info.

--- clients/python/media_storage/common.py
+++ clients/python/media_storage/common.py
@@ -192,7 +192,7 @@ def send_request(request, output=None, timeout=10.0):
             output.seek(0)
             return properties
         try:
-            if sys.version_info.major == 2: #Python 2.x, prior to 2.7.2, has a broken "fileno" attribute
+            if sys.version_info[0] == 2: #Python 2.x, prior to 2.7.2, has a broken "fileno" attribute
                 return (properties, mmap.mmap(response.fp._sock.fp.fileno(), 0, mmap.ACCESS_READ))
             return (properties, mmap.mmap(response.fileno(), 0, mmap.ACCESS_READ))
         except MemoryError:
