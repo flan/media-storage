6d2f1ffe29e3530c3543fe523b18723d83514334
flan <flan@localhost>
2012-04-09 16:02:46 -0600

    ...Downloading was already efficient.

--- clients/python/media_storage/common.py
+++ clients/python/media_storage/common.py
@@ -192,9 +192,7 @@ def send_request(request, output=None, timeout=10.0):
             output.seek(0)
             return properties
         try:
-            if sys.version_info[0] == 2: #Python 2.x, prior to 2.7.2, has a broken "fileno" attribute
-                return (properties, mmap.mmap(response.fp._sock.fp.fileno(), 0, mmap.ACCESS_READ))
-            return (properties, mmap.mmap(response.fileno(), 0, mmap.ACCESS_READ))
+            return (properties, response.read())
         except MemoryError:
             raise MemoryError("Insufficient memory to buffer data from storage")
             
